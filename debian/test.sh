cc38404941771163034f1dae338ac260b235ca6d
Simon McVittie <smcv@debian.org>
2017-10-12 22:48:54 +0100

    d/test.sh: Wrap build-time tests
    This makes test-suite failures non-fatal on architectures with known
    issues (downgrades severity of: #877428, #878284, #878285, #878286)
    
    Also skip the build-time tests if cross-compiling.

---
+++ debian/test.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+: "${SRCDIR:=./js/src}"
+: "${DEB_HOST_ARCH:=$(dpkg --print-architecture)}"
+
+if "$SRCDIR/js/src/js" -e 'print("Hello, world")'; then
+	echo "Smoke-test successful, continuing with full test suite"
+else
+	echo "Smoke-test failed: did interpreter initialization fail? (see #873778)"
+	exit 1
+fi
+
+if make -C "${SRCDIR}" -k check-jstests; then
+	echo "check-jstests successful"
+else
+	echo "check-jstests failed, checking whether to continue anyway..."
+
+	case "${DEB_HOST_ARCH}" in
+		(kfreebsd-i386|kfreebsd-amd64)
+			echo "Ignoring test failure, https://bugs.debian.org/878331"
+			;;
+		(mips64el)
+			echo "Ignoring test failure, https://bugs.debian.org/877428"
+			;;
+		(mips)
+			echo "Ignoring test failure, https://bugs.debian.org/878284"
+			;;
+		(ppc64el)
+			echo "Ignoring test failure, https://bugs.debian.org/878319"
+			;;
+		(s390x|powerpc64)
+			echo "Ignoring test failure, https://bugs.debian.org/878286"
+			;;
+		(powerpc|s390x|powerpc64)
+			echo "Ignoring test failure, https://bugs.debian.org/878285"
+			;;
+		(*)
+			echo "Test failure is considered serious, causing FTBFS"
+			exit 1
+	esac
+fi
