13a6e65841ea74ed8e604c6313d22632dc6ae7d3
Simon McVittie <smcv@debian.org>
2018-08-10 10:52:53 +0100

    Build with srcdir != builddir
    This matches the behaviour of GNOME's various continuous integration
    systems, and avoids a FTBFS when srcdir = builddir.

--- debian/test.sh
+++ debian/test.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 : "${SRCDIR:=./js/src}"
+: "${BUILDDIR:=./debian/build}"
 : "${DEB_HOST_ARCH:=$(dpkg --print-architecture)}"
 
-if "$SRCDIR/js/src/js" -e 'print("Hello, world")'; then
+if "$BUILDDIR/js/src/js" -e 'print("Hello, world")'; then
 	echo "Smoke-test successful, continuing with full test suite"
 else
 	echo "Smoke-test failed: did interpreter initialization fail? (see #873778)"
 	exit 1
 fi
 
-if make -C "${SRCDIR}" -k check-jstests; then
+if make -C "${BUILDDIR}" -k check-jstests; then
 	echo "check-jstests successful"
 else
 	echo "check-jstests failed, checking whether to continue anyway..."
