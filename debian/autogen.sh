276fdc6c8daa1f4d6b54113a59b19f7840c42ff7
Simon McVittie <smcv@debian.org>
2018-09-19 09:02:30 +0100

    Skip autoreconf for bundled ICU if we aren't going to build it anyway

--- debian/autogen.sh
+++ debian/autogen.sh
@@ -5,4 +5,6 @@ set -x
 
 autoconf2.13 old-configure.in > old-configure
 autoconf2.13 configure.in > configure
-( cd ../../intl/icu/source; autoreconf -fi --verbose )
+if [ "$WITH_SYSTEM_ICU" != yes ]; then
+    ( cd ../../intl/icu/source; autoreconf -fi --verbose )
+fi
