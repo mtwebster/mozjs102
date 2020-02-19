a2f602d50bf79b29f2c95d51da74811bfead51fa
Marco Trevisan (Treviño) <mail@3v1n0.net>
2020-02-19 13:24:37 +0100

    debian/tests.sh: Also run basic JIT tests
    We don't call make check-jit-test because that would run all the tests and
    we don't care about them all (like many wasm ones), so let's just run the
    basic from here without further patching of the Makefile.

--- debian/test.sh
+++ debian/test.sh
@@ -13,9 +13,16 @@ fi
 
 export HOME="$(mktemp -d -t mozjs-tests-home-XXXXXXXXXX)"
 
-if make -C "${BUILDDIR}" -k check-jstests; then
-	echo "check-jstests successful"
+if "${BUILDDIR}"/dist/bin/run-mozilla.sh \
+   "${BUILDDIR}"/_virtualenvs/init/bin/python -u \
+   "${SRCDIR}"/jit-test/jit_test.py \
+   --format=automation \
+   --no-slow \
+   --no-progress \
+   --timeout 600 \
+   "${BUILDDIR}"/dist/bin/js basic; then
+	echo "check-jit-test successful"
 else
-	echo "check-jstests failed"
+	echo "check-jit-test failed"
 	exit 1
 fi
