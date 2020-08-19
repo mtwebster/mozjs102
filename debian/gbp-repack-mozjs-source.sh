def610e008750ea8617baeb32544ea59c8466149
Marco Trevisan (Treviño) <mail@3v1n0.net>
2020-08-20 01:04:00 +0200

    gbp-repack-mozjs-source: Include ICU and changed files
    Some files are not included in the debian/*/master tree but are still
    kept in the orig file, so it makes sense to have them around, while
    others (Cargo.toml) gets modified in place by the mozilla mozjs
    generator script. Even if we want to use this version, unfortunately we
    need to do this via distro-patching or we'll get an archives mismatch.

--- debian/gbp-repack-mozjs-source.sh
+++ debian/gbp-repack-mozjs-source.sh
@@ -13,28 +13,40 @@ export STAGING="$GBP_TMP_DIR"
 export DIST=/dev/null
 export MOZJS_NAME="mozjs-repack"
 
+export RSYNC=${RSYNC:-rsync}
 # Don't reconfigure the source or create another archive
 export AUTOCONF=/bin/true
 export TAR=/bin/true
 
+EXTRA_FILES=(
+    Cargo.toml
+    intl/icu
+    intl/icu-patches
+    intl/icu_sources_data.py
+)
+
 srcpath=$(find "$GBP_TMP_DIR/" -mindepth 1 -maxdepth 1 -type d)
 "$srcpath"/js/src/make-source-package.sh
 
-mozjspath="$(find "$STAGING" -mindepth 1 -maxdepth 1 -type d -type d -name "$MOZJS_NAME"'-*')"
+mozjspath="$(find "$STAGING" -mindepth 1 -maxdepth 1 -type d -name "$MOZJS_NAME"'-*')"
 
 # Remove added files, could be kept if gbp would recreate the orig after us
 set +x
 shopt -s globstar
-for f in $mozjspath/**; do
+for f in "$mozjspath"/**; do
     f="${f#"$mozjspath/"}"
     if [ ! -e "$srcpath/$f" ]; then
         rm "$mozjspath/$f" && echo "Removing generated $mozjspath/$f"
     fi
 done
 
+for ((i = 0; i < ${#EXTRA_FILES[@]}; i++)); do
+    $RSYNC -a -q "$srcpath/${EXTRA_FILES[$i]}" "$mozjspath/$(dirname "${EXTRA_FILES[$i]}")/"
+done
+
 tmpout=$(mktemp /tmp/mozjs-debimport-XXXXXXXXX.diff)
 echo "Differencies found with orig saved at $tmpout, consider filtering them"
-diff -rq "$srcpath" "$mozjspath" > $tmpout || true
+diff -rq "$srcpath" "$mozjspath" > "$tmpout" || true
 
 rm -rf "$srcpath"
 mv -v "$mozjspath" "$srcpath"
